49e5b57481d2e4d871515d73760c89a33b023b15
unknown <alan.349@hotmail.com>
2021-06-10 21:27:23 -0300

    add useRef to retrieve inputs value

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,28 @@
-// eslint-disable-next-line
-import logo from './logo.svg';
+import { useRef } from 'react';
+
 import './App.css';
+import { sum } from './helpers/calculator';
 
 function App() {
+  const number1 = useRef(null);
+  const number2 = useRef(null);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    // console.log('submit');
+    sum(number1, number2);
+    // console.log('submit');
+  }
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={(event) => submitHandler(event)}> 
         <div>
           <label htmlFor="number1">Número 1</label>
-          <input type="number" name="number1" id="number1" placeholder="Insira o primeiro número" required />
+          <input type="number" name="number1" id="number1" placeholder="Insira o primeiro número" ref={number1} required />
         </div>
         <div>
           <label htmlFor="number2">Número 2</label>
-          <input type="number" name="number2" id="number2" placeholder="Insira o segundo número" required />
+          <input type="number" name="number2" id="number2" placeholder="Insira o segundo número" ref={number2} required />
         </div>
         <div>
           <button>Somar</button>
